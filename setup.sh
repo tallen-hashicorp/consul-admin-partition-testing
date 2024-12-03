5ac5922783f5e425b777c4bcd3fcc7b62bef589e
Tyler Allen <tyler.allen@hashicorp.com>
2024-12-03 10:18:07 +0000

    Add 1 Admin Partition Fixes #2

--- setup.sh
+++ setup.sh
@@ -10,7 +10,16 @@ kubectl apply -f 2-dc2
 
 kubectl apply -f 3-setup-peering
 
+echo ""
+echo "To Setup port forwarding use the following"
+echo "kubectl -n consul-monitoring port-forward services/grafana-service 3000:80"
+echo "kubectl -n consul-dc1 port-forward services/consul 8500:8500"
+echo "kubectl -n consul-dc2 port-forward services/consul 8500:8500"
+
+echo ""
 echo "Cluster building, waiting 2 minutes to get tokens"
+
+sleep 120
 
 echo "dc1: $(kubectl -n consul-dc1 get secrets consul-bootstrap-init-secret --template={{.data.SecretID}} | base64 -d)"
 echo "dc2: $(kubectl -n consul-dc2 get secrets consul-bootstrap-init-secret --template={{.data.SecretID}} | base64 -d)"
